ab451da18e00b530e6eec3b81749d3e7a94b65d8
Yana Wilkins <yav9774@gmail.com>
2017-08-03 16:43:49 -0400

    mystery word is now underscores

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
-const path = require('path') //Do I need it?
 
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
-const expressValidator = require('express-validator') //Do I need it?
+const expressValidator = require('express-validator')
 app.engine('mst', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mst')
@@ -20,16 +19,22 @@ const words = fs
   .toLowerCase()
   .split('\n')
 ////////////////////////////////////////////
-const guess = ['a', 'b', 'c'] //<---- what user post
+const wordToGuess = words[Math.floor(Math.random() * words.length)]
+const wordLength = wordToGuess.split('')
+let placeholder = wordLength.map(x => {
+  return '_'
+})
+
+const guess = [] //<---- what user post
 
 const guessed = [] //<----- will post in guessed
 
-app.get('/lose', req, res) =>{
-  res.render('/lose')
-}
+app.get('/lose', (req, res) => {
+  res.render('lose')
+})
 
 app.get('/', (req, res) => {
-  res.render('home', { guess, guessed })
+  res.render('home', { guess, guessed, placeholder })
 })
 // this 'talks' to the post in the function in home.mst
 app.post('/letters/add', function(req, res) {
